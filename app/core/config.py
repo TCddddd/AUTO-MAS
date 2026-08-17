45f3f11eac83bc33d5ab01e397757a1f2972997c
ClozyA <aoxuan233@gmail.com>
2026-08-17 21:33:59 +0800

    chore(version): 更新版本号至 v9.9.9-alpha.1

--- app/core/config.py
+++ app/core/config.py
@@ -131,7 +131,7 @@ except ImportError:
 
 
 class AppConfig(GlobalConfig):
-    VERSION = "v5.4.0-beta.7"
+    VERSION = "v9.9.9-alpha.1"
 
     def __init__(self) -> None:
         super().__init__()
